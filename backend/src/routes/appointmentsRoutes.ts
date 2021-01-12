95a2c945f759bf9bac4148c78e35c8ee4e2a323b
João Soares <jpcirqueira81@gmail.com>
2021-01-12 17:23:05 -0300

    adicionado rota de criação de appointment.

--- backend/src/routes/appointmentsRoutes.ts
+++ backend/src/routes/appointmentsRoutes.ts
@@ -1,14 +1,41 @@
 import { Router, request, response } from 'express';
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
+import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import User from '../models/user/User';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointemntsRouter = Router();
 
-appointemntsRouter.get('/', (request, response) => {
+appointemntsRouter.get('/', async (request, response) => {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-    const appointments = appointmentsRepository.find();
-
+    const appointments = await appointmentsRepository.find();
     return response.json(appointments);
+});
+
+appointemntsRouter.post('/', async (request, response) => {
+    try {
+        const { user_id, date } = request.body;
+
+        const parsedDate = parseISO(date);
+        const usersRepository = getRepository(User);
+        const appointmentsRepository = getCustomRepository(
+            AppointmentsRepository,
+        );
+
+        const user = await usersRepository.findOne({ where: { id: user_id } });
+        const createAppointment = new CreateAppointmentService();
+        if (!user) {
+            return response.status(401).json('User not found!');
+        }
+        const appointment = await createAppointment.execute({
+            user,
+            date: parsedDate,
+        });
+        return response.json(appointment);
+    } catch (error) {
+        return response.status(400).json(error);
+    }
 });
 
 export default appointemntsRouter;
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import usersRouter from './usersRoutes';
+import appointemntsRouter from './appointmentsRoutes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+routes.use('/appointment', appointemntsRouter);
 
 export default routes;
