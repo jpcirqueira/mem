6e94020d2ee946ef3ac1b6fc04f79939c1431efe
jpcirqueira <jpcirqueira81@gmail.com>
2021-04-11 23:05:39 -0300

    configure routes.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 
+import Routes from './routes';
+
 const App: React.FC = () => (
   <>
+    <Routes />
     <GlobalStyle />
   </>
 );
---
+++ web/src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Title } from './style';
+
+const Home: React.FC = () => (
+  <>
+    <Title>teste</Title>
+  </>
+);
+
+export default Home;
